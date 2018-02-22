f4a8315dcf1216fa405e6690eef73d4078315f33
Martin Musale <martinmshale@gmail.com>
2018-02-22 12:05:53 +0300

    Add url and class for bulk delete of todos

--- app/todos/urls.py
+++ app/todos/urls.py
@@ -5,6 +5,7 @@ from todos import views
 urlpatterns = {
     url(r'^$', views.TodoList.as_view(), name='list'),
     url(r'^(?P<pk>[0-9]+)/$', views.TodoDetail.as_view(), name='details'),
+    url(r'^delete/$', views.TodosDeleteAll.as_view(), name='delete_all')
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- app/todos/views.py
+++ app/todos/views.py
@@ -1,10 +1,11 @@
 
 from todos.serializers import TodoSerializer
 from todos.models import Todos
-from rest_framework import generics
-# from rest_framework_bulk import BulkDestroyAPIView
+from rest_framework import generics, response
+from rest_framework_bulk import BulkDestroyAPIView
 
 # TODO: Add delete all todos, add url to responses, enable CORS properly
+
 
 class TodoList(generics.ListCreateAPIView):
     queryset = Todos.objects.all()
@@ -13,6 +14,15 @@ class TodoList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save()
 
+
 class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Todos.objects.all()
     serializer_class = TodoSerializer
+
+
+class TodosDeleteAll(BulkDestroyAPIView):
+    queryset = Todos.objects.all()
+    serializer_class = TodoSerializer
+
+    def allow_bulk_destroy(self, qs, filtered):
+        return response.Response({'destroy': True})
